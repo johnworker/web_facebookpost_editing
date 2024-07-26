b5a5bdf73f217c1a6f34cc30bcc0ccaa7f961176
johnworker <besscomdark@gmail.com>
2024-07-26 13:53:16 +0800

    調整

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 window.addEventListener('load', function () {
-    var editor;
-    editor = ContentTools.EditorApp.get();
-    editor.init('*[data-editable]', 'data-name');
+    // 加載已保存的內容
+    if (localStorage.getItem('post-header')) {
+        document.querySelector('.post-header').innerHTML = localStorage.getItem('post-header');
+    }
+    if (localStorage.getItem('post-images')) {
+        document.querySelector('.post-images').innerHTML = localStorage.getItem('post-images');
+    }
+
+    // 保存變更
+    document.getElementById('saveButton').addEventListener('click', function () {
+        localStorage.setItem('post-header', document.querySelector('.post-header').innerHTML);
+        localStorage.setItem('post-images', document.querySelector('.post-images').innerHTML);
+        alert('變更已保存！');
+    });
 });
