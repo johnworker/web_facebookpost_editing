05babe0d5bcc4aed2b00b801f369a314bd524bca
johnworker <besscomdark@gmail.com>
2024-07-26 14:13:54 +0800

    調整

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// 更改文章和圖片
 window.addEventListener('load', function () {
     // 加載已保存的內容
     if (localStorage.getItem('post_header')) {
@@ -12,5 +14,23 @@ window.addEventListener('load', function () {
         localStorage.setItem('post_header', document.querySelector('.post_header').innerHTML);
         localStorage.setItem('post_images', document.querySelector('.post_images').innerHTML);
         alert('變更已保存！');
+    });
+
+    // 處理圖片替換和刪除
+    document.querySelectorAll('.post_images').forEach(function (img) {
+        img.addEventListener('click', function () {
+            if (confirm('是否要替換這張圖片？')) {
+                document.getElementById('imageUpload').click();
+                document.getElementById('imageUpload').onchange = function (event) {
+                    const reader = new FileReader();
+                    reader.onload = function () {
+                        img.src = reader.result;
+                    };
+                    reader.readAsDataURL(event.target.files[0]);
+                };
+            } else if (confirm('是否要刪除這張圖片？')) {
+                img.remove();
+            }
+        });
     });
 });
